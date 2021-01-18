b705a0e315da6dbb0a932ea1051b311d0e1d53ae
unknown <byoungsoo@lgcns.com>
2021-01-18 13:30:24 +0900

    Initial Commit

---
+++ src/main/java/com/example/demo/fruit/controller/SampleController.java
@@ -0,0 +1,34 @@
+package com.example.demo.fruit.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SampleController {
+	
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@GetMapping("/sample")
+	public String getFruit(Model model) {
+		Map<String, String> fruitMap = new HashMap<String, String>();
+
+		fruitMap.put("fruit1", "apple");
+		logger.info("info test");
+		fruitMap.put("fruit2", "kiwi");
+		logger.debug("debug test");
+		fruitMap.put("fruit3", "oragne");
+		logger.info("info test2");
+		
+
+		model.addAttribute("fruit", fruitMap);
+
+		return "sample/sample";
+	}
+
+}
